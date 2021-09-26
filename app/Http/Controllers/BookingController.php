a9352f6d31db21b19d6cda0a5c7a01a586e5ac69
LucanUK <github@lucanops.net>
2021-09-26 23:52:06 +0100

    Enable Garage mail alerts

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -50,8 +50,8 @@ class BookingController extends Controller
         $mailheader = "An MOT Booking for  " . $vehicledata->registrationNumber . " has been received";
         $mailuser = $userdata;
         $maildata = array('header' => $mailheader, 'user' => json_encode($userdata,true), 'vehicle' => json_encode($vehicledata,true), 'name' => $userdata['FirstName']);
-        $mailto = "booking@lucanops.net";
-        #$mailto = "service@sastestandrepair.co.uk";
+        #$mailto = "booking@lucanops.net";
+        $mailto = "service@sastestandrepair.co.uk";
         $mailsubject = "MOT Booking - " . $userdata['bookingdate'] . " @ " . $userdata['timeslot'];
         #dd(config('mail'));
         #dd($maildata);
